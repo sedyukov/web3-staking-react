5a582fe5345844bbd71a2a49f6f9e8d9e5255d1f
Yaroslav Sedyukov <sedyukovwork@gmail.com>
2022-06-22 14:11:25 +0700

    feature: connection of infura and wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,48 @@
-import React, { useEffect } from 'react';
-import { connectNode } from './web3';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { connectNode, connectWallet } from './web3';
 import './App.css';
 
 function App(): JSX.Element {
+  const [nodeStatus, setNodeStatus] = useState('');
+  const [chainId, setChainId] = useState(0);
   useEffect(() => {
-    const { ok, data } = connectNode();
-    console.log(ok, data);
+    const connect = async () => connectNode();
+    connect().then((resp) => {
+      setNodeStatus(resp.data.toString());
+    });
   }, []);
+  const handleConnectWallet = async () => {
+    await connectWallet().then((resp) => {
+      if (resp.ok) {
+        setChainId(Number(resp.data));
+      }
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="wallet">
+        <button type="submit" onClick={handleConnectWallet}>
+          wallet
+        </button>
+        <div>{nodeStatus}</div>
+        <div>{chainId}</div>
+      </div>
+      <div>
+        <div className="content">
+          <div className="content__right-row">
+            <div>TokenView</div>
+            <div>TokenForm</div>
+          </div>
+          <div className="content__left-row">
+            <div>TokenView</div>
+            <div>StackingInfo</div>
+            <div>ContractForm</div>
+          </div>
+        </div>
+        <div className="content scroll-area">
+          EventsTable
+        </div>
+      </div>
     </div>
   );
 }
